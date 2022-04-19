09e1954cac82908b65dd1023d5aac11c374f0f44
Brandon Bruno <babruno@aggies.ncat.edu>
2022-04-19 18:29:29 -0400

    update removed s

--- pii_data.py
+++ pii_data.py
@@ -8,7 +8,7 @@ class Pii(str):
     # https://regex101.com
     # https://www.w3schools.com/python/python_regex.asp
     def has_us_phone(self):
-        # Match a US phone number ddd-ddd-dddd ie 123-456-7890s
+        # Match a US phone number ddd-ddd-dddd ie 123-456-7890
         match = re.search(r'\d{3}-\d{3}-\d{4}', self)
         if match:
             return True
